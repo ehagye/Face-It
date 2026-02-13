79861a5fd82d6c052f96d87fb8e034b31d955306
Estelle <caitbenn22@icloud.com>
2026-02-12 21:24:01 -0500

    initial commit

---
+++ supabase-client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://evoqwkezqahsvctmopld.supabase.co",
+    "sb_publishable_OzhXMAGzO7kkMhHrOUPbQw_GKrJIPbM"
+);
+
